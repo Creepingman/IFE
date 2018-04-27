164ee477d9ef08d2b1a05f6dd5c6b8ba7f2672fd
creepingman <410436061@qq.com>
2018-04-28 00:10:27 +0100

    made a bit progress

--- data-manipulation.js
+++ data-manipulation.js
@@ -2,35 +2,46 @@
 * @Author: creepingman
 * @Date:   2018-04-27 17:07:26
 * @Last Modified by:   creepingman
-* @Last Modified time: 2018-04-27 23:10:26
+* @Last Modified time: 2018-04-28 00:08:51
 */
 var san = require('./node_modules/san/dist/san.js');
 
 var DataMani = san.defineComponent({
 	template: '<div>'
-		+'	<input type="text" value= "{= name =}" placeholder="please sign your name here"></br>'
-		+'	<input type="text" value= "{= age =}" placeholder="please tell me your age"></br>'
-		+'	<input type="text" value= "{= des =}" placeholder="pls introduce yourself a bit"></br>'
+		+'	<input type="text" value= "{= user.name =}" placeholder="please sign your name here"></br>'
+		+'	<input type="text" value= "{= user.age =}" placeholder="please tell me your age"></br>'
+		+'	<input type="text" value= "{= user.des =}" placeholder="pls introduce yourself a bit"></br>'
 		+'	<input type="button" class="button" value="Del message below" on-click="toggle"></br>'
-		+'	<p>name: {{= name =}}</p>'
-		+'	<p>Age: {{= age =}}</p>'
-		+'	<p>Description: {{= des =}}</p>'
+		/* Employ element-'span' to add additional effects for the program */
+		+'	<p>name: <span style= "text-decoration: underline">{{= user.name =}}</span></p>'
+		+'	<p>Age: <span style= "text-decoration: underline">{{= user.age =}}</span></p>'
+		+'	<p>Description: <span style= "text-decoration: underline">{{= user.des =}}</span></p>'
 		+'	</div>',
 
+			            /* Bad version */
 	/* Initial letter should be capitalized to define the data type */
-	initData: {
+	/*initData: {
 		return: {
 			name: String,
 			age: Number,
 			des: String
 		}
+	},*/
+
+	// better version (reference: https://github.com/Creepingman/IFE/blob/master/data-manipulation.js)
+	initData: function() {
+		return {
+			user: {
+				name: '',
+				age: null,
+				des: ''
+			}
+		}
 	},
 
 	/* pls pay more attention on the punctuation mark whenever to coding */
 	toggle: function(){
-		this.data.set('name', '');
-		this.data.set('age', '');
-		this.data.set('des', '');
+		this.data.set('user', {name: '', age: null, des: ''})
 	}
 });
 
